750bd099e0ad0be1cc964d7133bcca2077779e2e
Jim Cooke and Marilyn Carlin <jim@>
2017-02-06 20:25:04 -0800

    add box model elements to image.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,11 @@ $(document).ready(function() {
     $("#manatee-hidden").slideToggle();
   });
 
+  $("#manatee-click").click(function() {
+    $("img").removeClass();
+    $("img").addClass("manatee-image");
+  });
+
   $("button#default").click(function() {
     $("body").removeClass();
     $("body").addClass("light-background");
